1f8710a3faad9c119884ea67094792b40d09a4b4
CptArthur <59366211+CptArthur@users.noreply.github.com>
2023-12-09 14:31:04 +0100

    0.4.10

--- Ares at War part 1/Data/Scripts/Ares at War/AaW_Core.cs
+++ Ares at War part 1/Data/Scripts/Ares at War/AaW_Core.cs
@@ -23,7 +23,7 @@ namespace AresAtWar.SessionCore
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
     public class AaWSession : MySessionComponentBase
     {
-        public static string ModVersion = "0.4.9.8";
+        public static string ModVersion = "0.4.10";
 
         public static MESApi MESApi;
         public int counter = 0;
